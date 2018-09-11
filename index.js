e9c5ad2fe637c00d48135ac54e4fb5c5336ce129
Ben Parsons <ben@bpulse.co.uk>
2018-09-11 15:46:42 +0100

    get repo details

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const octokit = require('@octokit/rest')();
+var moment = require('moment');
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('db.db', sqlite3.OPEN_READWRITE);
 
@@ -18,6 +19,7 @@ var repos = [
 
 var issues = true;
 var commits = true;
+var details = false;
 
 // var repoLastUpdated = db.get(`SELECT updated FROM issues WHERE owner = '${owner}' AND repo = '${repo}' ORDER BY updated desc`);
 // if (repoLastUpdated) { repoLastUpdated = repoLastUpdated.updated; }
@@ -35,6 +37,7 @@ async function init() {
         });
     }
 
+    /* COMMITS */
     if (commits) {
         repos.forEach(async repo => {
             var response = await octokit.repos.getCommits({
@@ -44,6 +47,40 @@ async function init() {
             });
             response.data.forEach(commit => {
                 tryUpdateCommit(repo.owner, repo.repo, commit);
+            });
+        });
+    }
+
+    if (details) {
+        repos.forEach(async repo => {
+            var response = await octokit.repos.get({
+                owner: repo.owner,
+                repo: repo.repo
+            });
+            var insertSql = `
+            INSERT INTO repo_details(
+                owner, repo, stargazers_count, watchers_count, forks_count, date
+            )
+            VALUES (
+                '${repo.owner}',
+                '${repo.repo}',
+                ${response.data.stargazers_count},
+                ${response.data.watchers_count},
+                ${response.data.forks_count},
+                '${moment().format()}'
+            )
+            `;
+            db.run(insertSql, function(error) {
+                if (error) {
+                    console.log(error);
+                    process.exit(1);
+                }
+                console.log(this);
+                if (this.changes !== 1) {
+                    console.log("insert failed:");
+                    console.log(insertSql);
+                    process.exit(1);
+                }
             });
         });
     }
